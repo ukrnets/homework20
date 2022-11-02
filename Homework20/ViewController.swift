1e466c404b7c39b8be0b3ac41dcef2d36b5ac93f
Artem Grabovski <art.grabovski@gmail.com>
2022-11-02 16:59:53 +0100

    Initial Commit

---
+++ Homework20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework20
+//
+//  Created by Darya Grabowskaya on 2.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
